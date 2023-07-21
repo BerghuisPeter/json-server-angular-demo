9385a4d3a70e609c0282c0c87df7e0b19599f76b
peter <peter.berghuis_13@hotmail.com>
2023-07-22 00:53:00 +0200

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mock-backend-responses-in-angular';
+
+  // @ts-ignore
+  content: Object;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  get() {
+    this.httpClient.get("http://localhost:3000/api/pandas")
+      .subscribe(res => this.content = res);
+  }
+
+  post() {
+    this.httpClient.post("http://localhost:3000/api/pandas",
+      {
+        "name": "test",
+        "sex": "F",
+        "birthDate": "09/03/2009",
+        "currentLocation": "no Where"
+      })
+      .subscribe(res => this.content = res);
+  }
+
+  put() {
+    this.httpClient.put("http://localhost:3000/api/pandas/4",
+      {
+        "name": "YOOOOOO",
+        "sex": "F",
+        "birthDate": "09/03/2009",
+        "currentLocation": "PUT"
+      })
+      .subscribe(res => this.content = res);
+  }
+
+  delete() {
+    this.httpClient.delete("http://localhost:3000/api/pandas/4")
+      .subscribe(res => this.content = res);
+  }
+
+  searchByName() {
+    this.httpClient.get("http://localhost:3000/api/pandas?name=Numi")
+      .subscribe(res => this.content = res);
+  }
+
+  proxyGet() {
+    this.httpClient.get("http://localhost:3000/api/pandas")
+      .subscribe(res => this.content = res);
+  }
+
 }
